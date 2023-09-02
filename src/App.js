03410c75526a86af168340d1a409c3c3b1b541bf
OMARLAHRECH <omarlahresh@gmail.com>
2023-09-02 16:05:34 +0100

    omar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbarr from './components/Navbarr';
+import Hedi from './components/Hedi';
+import Cardd from './components/Cardd';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbarr></Navbarr>
+      <Hedi></Hedi>
+      <Cardd></Cardd>
+      <Cardd></Cardd>
+      <Cardd></Cardd>
+    </div>
+  );
+}
+
+export default App;
